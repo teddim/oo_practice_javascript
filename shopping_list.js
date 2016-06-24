22c0d46dc926a160c1ca460a666a617d32b6fc52
Teddi Maull <teddimaull@Teddis-MacBook-Pro.local>
2016-06-24 11:47:04 -0600

    my solution

--- shopping_list.js
+++ shopping_list.js
@@ -1,7 +1,26 @@
 var ShoppingList = function(budget){
-
+  this.budget = budget;
+  this.contents = [];
 
 }
 
+ShoppingList.prototype.addItem = function(item){
+  if(this.budget < item.price){
+    return "sorry, you can't have that";
+  }else{
+    this.budget -= item.price;
+    this.contents.push(item);
+  }
+};
+
+ShoppingList.prototype.total = function(){
+  return this.contents.reduce(function(prev,curr){
+    return prev + curr.price;
+  },0);
+};
+
+ShoppingList.prototype.swap = function(item1, item2){
+  this.contents[this.contents.indexOf(item1)] = item2;
+}
 
 module.exports = ShoppingList;
